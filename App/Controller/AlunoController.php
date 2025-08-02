2c9f950fddeb3354d6ca30344b8b331d66cd07b3
AdenorGit <adenor@gmail.com>
2025-08-02 15:13:29 -0300

    Adição routes.php

--- App/Controller/AlunoController.php
+++ App/Controller/AlunoController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 class AlunoController
 {
-    public function teste()
+    public static function cadastro()
     {
-        echo 'Teste';
+        echo 'Mostrar formulario a depender...';
+    }
+
+    public static function listar()
+    {
+        echo 'Listagem de alunos';
     }
 }
--- index.php
+++ index.php
@@ -2,12 +2,4 @@
 
 include 'config.php';
 include 'autoload.php';
-
-use App\Controller\AlunoController;
-use App\Model\Aluno;
-
-$oo = new AlunoController();
-$oo->teste();
-
-$m = new Aluno;
-$m->teste();
+include 'routes.php';
